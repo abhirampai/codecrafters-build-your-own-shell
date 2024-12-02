291feec943ab8986c462d29056f7861527d35b72
Abhiram Pai <abhirampai1999@gmail.com>
2024-12-02 19:55:35 +0530

    Passing stage run a program

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,13 +6,29 @@ import (
 	"os"
   "strings"
   "slices"
+  "os/exec"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
+var pathEnv = os.Getenv("PATH")
 
 func commandNotFound(command string) {
   fmt.Println(command + ": not found")
+}
+
+func findExecutablePath(command string) (executablePath string, pathFound bool) {
+  pathsToCheck := strings.Split(pathEnv, ":")
+  pathFound = false
+
+  for i := 0; i < len(pathsToCheck); i++ {
+    executablePath = pathsToCheck[i] + "/" + command
+    if _, err := os.Stat(executablePath); err == nil {
+      pathFound = true
+      break
+    }
+  }
+  return
 }
 
 func main() {
@@ -40,31 +56,28 @@ func main() {
           fmt.Println(strings.Join(args, " "))
       case "type":
         typeCommand := args[0]
-        pathEnv := os.Getenv("PATH")
 
         if slices.Contains(builtInCommands, typeCommand) {
           fmt.Println(typeCommand + " is a shell builtin")
         } else if len(pathEnv) > 0 {
-          pathsToCheck := strings.Split(pathEnv, ":")
-          pathFound := false
+          execPath, pathFound := findExecutablePath(typeCommand)
 
-          for i := 0; i < len(pathsToCheck); i++ {
-            executablePath := pathsToCheck[i] + "/" + typeCommand
-            if _, err := os.Stat(executablePath); err == nil {
-              fmt.Println(typeCommand + " is " + executablePath)
-              pathFound = true
-              break
-            }
-          }
-
-          if !pathFound {
+          if pathFound {
+            fmt.Println(typeCommand + " is " + execPath)
+          } else {
             commandNotFound(typeCommand)
           }
         } else {
           commandNotFound(typeCommand)
         } 
       default:
-        commandNotFound(command)
+        if execPath, pathFound := findExecutablePath(command); pathFound {
+          cmd := exec.Command(execPath, args...)
+          cmdOutput, _ := cmd.Output()
+          fmt.Print(string(cmdOutput))
+        } else {
+          commandNotFound(command)
+        }
     }
   }
 }
